b7f7a02168ddcf9e6959f707a782e02ed4dea915
Arif <hossaininfo@ymail.com>
2021-10-13 01:06:33 +0200

    Empty menu created

--- src/menu/MainMenu.java
+++ src/menu/MainMenu.java
@@ -1,8 +1,18 @@
 package menu;
 
+import java.util.Scanner;
+
 public class MainMenu {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("********************************");
+        System.out.println("1. Multiplication");
+        System.out.println("2. Multiply");
+        System.out.println("3. Division");
+        System.out.println("4. Addition");
+        System.out.println("********************************");
+        System.out.print("Please Make A Choice: ");
+        int choice = sc.nextInt();
 
 
     }
