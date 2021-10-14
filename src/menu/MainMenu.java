8c4e2b640c41db09c863bdef293d826e5b26b69b
Arif <hossaininfo@ymail.com>
2021-10-14 09:35:32 +0200

    a boolean variable call repeat created for loop menu, case 0 added in switch.

--- src/menu/MainMenu.java
+++ src/menu/MainMenu.java
@@ -7,26 +7,32 @@ import static method.CalculatorMethoder.*;
 public class MainMenu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("********************************");
-        System.out.println("1. Multiplication");
-        System.out.println("2. Multiply");
-        System.out.println("3. Division");
-        System.out.println("4. Addition");
-        System.out.println("********************************");
-        System.out.print("Please Make A Choice: ");
-        int choice = sc.nextInt();
-        switch (choice){
-            case 1:
-                multiplication();
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            default:
-                System.out.println("Wrong choice! Please choose from menu.");
+        boolean repeat = true;
+        while (repeat) {
+            System.out.println("********************************");
+            System.out.println("1. Multiplication");
+            System.out.println("2. Multiply");
+            System.out.println("3. Division");
+            System.out.println("4. Addition");
+            System.out.println("********************************");
+            System.out.print("Please Make A Choice: ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 0:
+                    repeat = false;
+                    break;
+                case 1:
+                    multiplication();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Wrong choice! Please choose from menu.");
+            }
         }
 
 
